ef25e3a5fcab6e32739b668544ee31627df55e4c
Jithendra <jithendrads@gmail.com>
2021-01-02 19:18:51 +0530

    Created Queue and published message to rabbitMq queue

---
+++ rabbitmqdemo/src/main/java/com/jithendra/rabbitmqdemo/RabbitmqdemoApplication.java
@@ -0,0 +1,27 @@
+package com.jithendra.rabbitmqdemo;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqdemoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqdemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		SimpleMessage simpleMessage=new SimpleMessage("FirstMessage","Simple description");
+		rabbitTemplate.convertAndSend("TestExchange","testRouting",simpleMessage);
+		
+	}
+
+}
---
+++ rabbitmqdemo/src/main/java/com/jithendra/rabbitmqdemo/SimpleMessage.java
@@ -0,0 +1,35 @@
+package com.jithendra.rabbitmqdemo;
+
+import java.io.Serializable;
+
+public class SimpleMessage implements Serializable{
+	private String name;
+	private String description;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public SimpleMessage(String name, String description) {
+		this.name = name;
+		this.description = description;
+	
+	}
+	public SimpleMessage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+
+}
