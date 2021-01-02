7d17d3e7c24d62456b76793927bab939c9800a6c
Jithendra <jithendrads@gmail.com>
2021-01-02 19:19:27 +0530

    created queue and published message to rabbitMq queue

---
+++ rabbitmq-listener-project/src/main/java/com/jithendra/rabbitmqlistenerproject/RabbitmqListenerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jithendra.rabbitmqlistenerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqListenerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqListenerProjectApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-listener-project/src/test/java/com/jithendra/rabbitmqlistenerproject/RabbitmqListenerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jithendra.rabbitmqlistenerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqListenerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
